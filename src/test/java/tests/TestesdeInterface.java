9508d4227b15985395a77a57aff5572427b4446d
gustavo.wesley <gustavo.wesley@senior.com.br>
2020-10-15 16:18:24 -0300

    Finalizando o teste

--- src/test/java/tests/TestesdeInterface.java
+++ src/test/java/tests/TestesdeInterface.java
@@ -17,8 +17,32 @@ public class TestesdeInterface {
 	WebDriver navegador = new ChromeDriver();
 	navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	navegador.get("https://www.webmotors.com.br/carros/estoque?tipoveiculo=carros&estadocidade=estoque");
-	navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div/div/a[4]/small")).click();;
+	navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div/div/a[4]/small")).click();
+	navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div[2]/div[2]")).click();
+	navegador.findElement(By.linkText("CITY")).click();
+	navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div[2]/div[3]")).click();
+	navegador.findElement(By.linkText("1.5 DX 16V FLEX 4P AUTOMÁTICO")).click();	
+	
+	
+
 	
 	}
+	
+
+		
+		@Test
+		public void Teste002() {
+		System.setProperty("webdriver.chrome.driver", "D:\\DEV2\\Workspace\\chromedriver.exe");
+		WebDriver navegador = new ChromeDriver();
+		navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		navegador.get("https://www.webmotors.com.br/carros/estoque/?IdRevendedor=3834764&TipoVeiculo=carros&anunciante=concession%C3%A1ria%7Cloja");
+		navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div/div/a[4]/small")).click();
+		navegador.findElement(By.xpath("//div[@id='root']/main/div/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/div[2]/div[2]")).click();
+		navegador.findElement(By.linkText("CITY")).click();
+		
+		
+
+		
+		}
 
 }
